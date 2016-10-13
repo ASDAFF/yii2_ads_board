a61499a0b59cefecfa7e689caacb64afef3727b9
Valeriy Pinchuk <morfeimail@gmail.com>
2016-10-13 22:54:54 +0300

    add methods, fix

--- common/models/Adsense.php
+++ common/models/Adsense.php
@@ -1,5 +1,6 @@
 <?php
 namespace common\models;
+use yii\data\ActiveDataProvider;
 use yii\db\ActiveRecord;
 use common\models\Category;
 use common\models\User;
@@ -16,6 +17,10 @@ class Adsense extends ActiveRecord{
     public $city;
     public $category;
     public $preview_img;
+    public $ads_url;
+
+    public $city_check;
+
     public static function tableName() {
         return '{{%adsense}}';
     }
@@ -23,12 +28,29 @@ class Adsense extends ActiveRecord{
    public function getCategory(){
         return $this->hasOne(Category::className(), ['id' => 'category_id']);
    }
+
+    /*
+    public function rules(){
+        return[
+            [['title','description','category_id','city_id','user_id','publish_status'], 'required'],
+        ];
+    }
+    */
+
+    public function attributeLabels(){
+        return [
+            'title' => 'Заголовок',
+            'description' => 'Описание',
+            'category_id' => 'Категория',
+            'city_id' => 'Город'
+        ];
+    }
+
     /*
     public function getUser(){
         return $this->hasOne(User::className(), ['id' => 'user_id']);
     }
     */
-
     public function getUserInfo(){
         return $this->hasOne(UserInfo::className(), ['id' => 'user_id']);
     }
@@ -40,6 +62,9 @@ class Adsense extends ActiveRecord{
     public function getImage(){
         return $this->hasOne(Image::className(), ['ads_id' => 'id']);
     }
+    public function getFavorite(){
+        return $this->hasMany(Favorite::className(), ['ads_id' => 'id']);
+    }
 
 
     public function getAdsenseList(){
@@ -48,8 +73,7 @@ class Adsense extends ActiveRecord{
             ->joinWith(['city','category','image'])
             ->select(['adsense.title','adsense.state', 'adsense.cost','adsense.date_publish',
             'adsense.id','city' => 'city.name','category' => 'category.name', 'preview_img' => 'images.path'])
-            ->where('publish_status = 1')
-            ->orderBy('date_publish DESC');
+            ->where(['publish_status' => 1]);
     }
 
     public function getAdsense($id){
@@ -62,4 +86,86 @@ class Adsense extends ActiveRecord{
             ->where(['adsense.id' => $id])
             ->one();
     }
+
+    public function getAdsenseByCategory($category){
+        $category_id = Category::find()
+            ->select('category.id')
+            ->where(['category.name' => $category])
+            ->one();
+
+        return Adsense::find()
+            ->joinWith(['city','category','image'])
+            ->select(['adsense.title','adsense.state', 'adsense.cost','adsense.date_publish',
+                'adsense.id','city' => 'city.name','category' => 'category.name', 'preview_img' => 'images.path'])
+            ->where(['adsense.category_id' => $category_id->id]);
+    }
+
+    public function getAdsenseFavorite($user_id){
+        $favorite = Favorite::find()
+            ->select('favorite.ads_id')
+            ->where(['favorite.user_id' => $user_id])
+            ->all();
+        $searchD = [];
+        foreach($favorite as $data){
+            $searchD[] = $data->ads_id;
+        }
+        $data = new ActiveDataProvider([
+            'query' => Adsense::find()
+                    ->joinWith(['city','category','image'])
+                    ->select(['adsense.title','adsense.state', 'adsense.cost','adsense.date_publish',
+                        'adsense.id','city' => 'city.name','category' => 'category.name', 'preview_img' => 'images.path'])
+                    ->where(['adsense.id' => $searchD]),
+            'pagination' => [
+                'pageSize' => 10
+            ]
+        ]);
+        return $data;
+
+    }
+
+    public function getAdsenseListByUserId($id){
+        $data = new ActiveDataProvider([
+            'query' => Adsense::find()
+                    ->joinWith(['city','category','image'])
+                    ->select(['adsense.title','adsense.state', 'adsense.cost','adsense.date_publish',
+                        'adsense.id','city' => 'city.name','category' => 'category.name', 'preview_img' => 'images.path'])
+                    ->where(['adsense.user_id' => $id]),
+            'pagination' => [
+                'pageSize' => 10
+            ]
+        ]);
+        return $data;
+    }
+
+    public function searchArticles($params){
+        $articles = Adsense::find()
+            ->joinWith(['city','category','image'])
+            ->select(['adsense.title','adsense.state', 'adsense.cost','adsense.date_publish',
+            'adsense.id','city' => 'city.name','category' => 'category.name', 'preview_img' => 'images.path'])
+            ->where(['publish_status' => 1])
+            ->andWhere(['like','title',$params['searchQ']]);
+
+        if($params['city_id'] != null){
+            $articles = $articles->andWhere(['adsense.city_id' => $params['city_id']]);
+        }
+
+        if($params['category_id'] != ""){
+            $articles = $articles->andWhere(['adsense.category_id' => $params['category_id']]);
+        }
+
+        if($params['onlyFoto'] == 1){
+            $articles = $articles->andWhere(['is not', 'images.path', null]);
+        }
+
+        if($params['minCost'] != null){
+            $articles = $articles->andWhere(['>', 'cost', $params['minCost'] - 1]);
+        }
+
+        if($params['maxCost'] != null){
+            $articles = $articles->andWhere(['<', 'cost', $params['maxCost']]);
+        }
+
+        return $articles;
+    }
+
 }
--- common/models/Category.php
+++ common/models/Category.php
@@ -14,4 +14,39 @@ class Category extends ActiveRecord{
         return $this->hasMany(Adsense::className(), ['category_id' => 'id']);
     }
 
+    public function getCategorysByParent($id){
+        return $this
+            ->find()
+            ->select('name')
+            ->where(['parent_id' => $id])
+            ->orderBy('name DESC')
+            ->all();
+    }
+
+    public function getCategoryList(){
+        $categorys = Category::find()
+            ->all();
+
+        $date = array();
+        $i = 0;
+
+        foreach($categorys as $cat){
+            if($cat->parent_id == 0){
+                $date[$i][] = $cat;
+                $i++;
+            }
+        }
+
+        foreach($categorys as $cat){
+            for($i=0; $i < count($date);$i++){
+                if($date[$i][0]->id == $cat->parent_id){
+                    $date[$i][] = $cat;
+                }
+            }
+        }
+
+        return $date;
+
+    }
+
 }
