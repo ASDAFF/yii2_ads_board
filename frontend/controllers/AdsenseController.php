bf97da539c29d743df73a8a3f9b256b991a91aea
Valeriy Pinchuk <morfeimail@gmail.com>
2016-10-13 22:55:58 +0300

    add/fix actions

--- frontend/controllers/AdsenseController.php
+++ frontend/controllers/AdsenseController.php
@@ -1,22 +1,45 @@
 <?php
 namespace frontend\controllers;
 
+use common\models\Category;
 use common\models\City;
+use frontend\models\SearchForm;
 use Yii;
+use yii\base\Exception;
+use yii\data\Sort;
 use yii\web\Controller;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 use yii\data\Pagination;
 
 use common\models\Adsense;
+use yii\web\HttpException;
 
 class AdsenseController extends Controller{
 
-    public function actionIndex(){
+    public function actionIndex($category = null,$date = null){
         $adsense = new Adsense();
-        //$city = new City();
-        //$list = $city->fin
-        $list = $adsense->getAdsenseList();
+        $sort = new Sort([
+            'attributes' => [
+                'date_publish' => [
+                    'asc' => ['date_publish' => SORT_ASC],
+                    'desc' => ['date_publish' => SORT_DESC],
+                    'default' => SORT_DESC,
+                    'label' => 'Дата публикации',
+                ],
+                'cost' => [
+                    'asc' => ['cost' => SORT_ASC],
+                    'desc' => ['cost' => SORT_DESC],
+                    'label' => 'Цена',
+                ]
+            ]
+        ]);
+
+        if($category != null){
+            $list = $adsense->getAdsenseByCategory($category);
+        }else{
+            $list = $adsense->getAdsenseList();
+        }
 
         $pagination = new Pagination(
             [
@@ -25,21 +48,82 @@ class AdsenseController extends Controller{
             ]
         );
 
+        $list = $list->orderBy($sort->orders);
+
         $list = $list
             ->offset($pagination->offset)
             ->limit($pagination->limit)
             ->all();
+
         return $this->render('index',[
             'list' => $list,
             'pagination' => $pagination,
+            'sort' => $sort,
+        ]);
+    }
+
+    public function actionSearch($date = null){
+        $adsense = new Adsense();
+        $model = new SearchForm();
+        $data = $_GET['data'];
+        $list = $adsense->searchArticles($data);
+
+        $sort = new Sort([
+            'attributes' => [
+                'date_publish' => [
+                    'asc' => ['date_publish' => SORT_ASC],
+                    'desc' => ['date_publish' => SORT_DESC],
+                    'default' => SORT_DESC,
+                    'label' => 'Дата публикации',
+                ],
+                'cost' => [
+                    'asc' => ['cost' => SORT_ASC],
+                    'desc' => ['cost' => SORT_DESC],
+                    'label' => 'Цена',
+                ]
+            ]
+        ]);
+
+        $pagination = new Pagination(
+            [
+                'defaultPageSize' => 10,
+                'totalCount' => $list->count(),
+            ]
+        );
+
+        $list = $list->orderBy($sort->orders);
+
+        $list = $list
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index',[
+            'list' => $list,
+            'pagination' => $pagination,
+            'sort' => $sort,
+
+
         ]);
     }
 
     public function actionCurrentAdsense($id){
         $adsense = new Adsense();
         $item = $adsense->getAdsense($id);
+
         return $this->render('current-adsense',[
             'adsense' => $item,
         ]);
     }
+
+    public function actionCategoryList(){
+        $category = new Category();
+        $list = $category->getCategoryList();
+
+        return $this->render('category-list',[
+            'list' => $list,
+        ]);
+    }
+
+
 }
--- frontend/controllers/ProfileController.php
+++ frontend/controllers/ProfileController.php
@@ -1,7 +1,132 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: work-oc
- * Date: 27.09.16
- * Time: 20:04
- */ 
+namespace frontend\controllers;
+
+use common\models\Image;
+use Faker\Provider\DateTime;
+use frontend\models\AdsenseForm;
+use Yii;
+use yii\base\Controller;
+use yii\data\ActiveDataProvider;
+use yii\data\Pagination;
+use common\models\Adsense;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+
+class ProfileController extends Controller{
+
+    public function behaviors(){
+        return[
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['my-adsense','add-adsense','delete','view','favorite'],
+                        'allow' => 'true',
+                        'roles' => ['@'],
+                    ],
+                ]
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['get','post'],
+                ]
+            ]
+        ];
+
+    }
+
+    public function actionMyAdsense(){
+        $adsense = new Adsense();
+        $dataProvider = new ActiveDataProvider([
+            'query' => Adsense::find(),
+            'pagination' => [
+                'pageSize' => 10,
+            ]
+        ]);
+
+        $user_id = Yii::$app->user->id;
+        $list = $adsense->getAdsenseListByUserId($user_id);
+
+        /*
+        $pagination = new Pagination(
+            [
+                'defaultPageSize' => 10,
+                'totalCount' => $list->count(),
+            ]
+        );
+
+        $list = $list
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+        */
+
+        return $this->render('my-adsense',[
+            'dataProvider' => $list,
+        ]);
+    }
+
+    public function actionAddAdsense(){
+        $model = new AdsenseForm();
+        $image = new Image();
+
+        $dir = Yii::getAlias('@frontend/web/img/');
+        if(isset($_POST['AdsenseForm'])){
+            $_POST['AdsenseForm']['user_id'] = Yii::$app->user->id;
+            $_POST['AdsenseForm']['publish_status'] = 1;
+            $_POST['AdsenseForm']['date_publish'] = date('Y-m-d H:i:s');
+            $model->attributes = Yii::$app->request->post('AdsenseForm');
+            if($model->save()){
+                $model->img_list = UploadedFile::getInstances($model,'img_list');
+                if($model->uploadList($model->id)){
+                    echo 'successX1';
+                }
+                $model->preview_img = UploadedFile::getInstance($model,'preview_img');
+                if(isset($model->preview_img)){
+                    if($model->uploadOne($model->id)){
+                        echo 'successX2';
+                    }
+                }
+                return Yii::$app->getResponse()->redirect(['profile/my-adsense']);
+            }
+        }
+        return $this->render('add-adsense',[
+            'model' => $model,
+
+        ]);
+    }
+
+    public function actionDelete($id = null){
+        $adsense = Adsense::findOne($_GET['id']);
+
+        if($adsense == null){
+            Yii::$app->session->setFlash('AdsenseDeletedError');
+            Yii::$app->getResponse()->redirect(['profile/my-adsense']);
+        }
+        $adsense->delete();
+
+        Yii::$app->session->setFlash('AdsenseDeleted');
+        Yii::$app->getResponse()->redirect(['profile/my-adsense']);
+    }
+
+    public function actionView(){
+        $adsense = new Adsense();
+        $data = $adsense->getAdsense($_GET['id']);
+
+        return $this->render('view',[
+            'data' => $data,
+        ]);
+    }
+
+    public function actionFavorite(){
+        $adsense = new Adsense();
+        $data = $adsense->getAdsenseFavorite(Yii::$app->user->id);
+
+        return $this->render('favorite',[
+            'data' => $data,
+        ]);
+    }
+
+}
